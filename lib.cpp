3bb8ae663657fb493025e306bd05a65d43da19a9
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-12-01 08:40:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include "lib.h"
+using namespace std;
+bool scm(int a, int b) {
+    a=abs(a);
+    b=abs(b);
+    if (a==0 || a==1)
+        return false;
+    int rest = a % --b;
+    if (rest==0 && b>1){
+        return false;
+    }else if(b!=1)
+        return scm(a,b);
+    else
+        return true;
+    }
