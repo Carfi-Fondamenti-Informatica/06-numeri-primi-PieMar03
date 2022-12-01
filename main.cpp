bdba14ff56e36f649f67a669ba017cc445fedc5a
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-12-01 08:42:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  int a=0;
+  cin>>a;
+  if (scm(a,a)){
+        cout << "numero primo";
+    }else {
+        cout << "numero non primo";
+    }
   return 0;
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
